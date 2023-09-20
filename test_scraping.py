21cbb5b06d34b4ff84156a56640ac2d8616713dd
Genghis Capella <gcapella@gs1.org.ar>
2023-09-20 11:33:58 -0300

    Creando la primera version de DolarArg

--- test_scraping.py
+++ test_scraping.py
@@ -1,37 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
 
-#------------------------------
+def dolarArg():
+    # URL de la página web
+    url = 'https://www.cronista.com/MercadosOnline/dolar.html'
+    
+    try:
+        # Realizar la solicitud GET
+        response = requests.get(url)
+        response.raise_for_status()  # Lanzar una excepción si la solicitud falla
 
-url = 'https://www.cronista.com/MercadosOnline/dolar.html'
-response = requests.get(url)
+        # Crear un objeto BeautifulSoup
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-#-----------------------------
+        # Buscar los elementos de compra y venta
+        buy_elements = soup.find_all('div', class_='buy-value')
+        sell_elements = soup.find_all('div', class_='sell-value')
 
-soup = BeautifulSoup(response.text, 'html.parser')
+        # Crear un diccionario para almacenar los valores
+        dolar_info = {'Dolar BNA': {}, 'Dolar Blue': {}, 'Dolar MEP': {}}
 
-# Busca todos los elementos <div> con la clase "buy-value"
-buy_elements = soup.find_all('div', class_='buy-value')
+        # Extraer los valores de compra si están disponibles
+        if buy_elements:
+            dolar_info['Dolar BNA']['Compra'] = buy_elements[0].text.strip()
+            dolar_info['Dolar Blue']['Compra'] = buy_elements[1].text.strip()
+            dolar_info['Dolar MEP']['Compra'] = buy_elements[4].text.strip()
 
-# Busca todos los elementos <div> con la clase "sell-value"
-sell_elements = soup.find_all('div', class_='sell-value')
+        # Extraer los valores de venta si están disponibles
+        if sell_elements:
+            dolar_info['Dolar BNA']['Venta'] = sell_elements[0].text.strip()
+            dolar_info['Dolar Blue']['Venta'] = sell_elements[1].text.strip()
+            dolar_info['Dolar MEP']['Venta'] = sell_elements[5].text.strip()
 
-# Crea un diccionario para almacenar los valores
-dolar_info = {'Dolar BNA': {},'Dolar Blue': {},'Dolar MEP': {}}
+        # Devolver el diccionario con la información
+        return dolar_info
 
-# Verifica si se encontraron elementos en la búsqueda de "buy-value"
-if buy_elements:
-    # El primer elemento de la lista de "buy-value" es el valor de "Compra"
-    dolar_info['Dolar BNA']['Compra'] = buy_elements[0].text.strip()
-    dolar_info['Dolar Blue']['Compra'] = buy_elements[1].text.strip()
-    dolar_info['Dolar MEP']['Compra'] = buy_elements[4].text.strip()
+    except requests.exceptions.RequestException as e:
+        print(f"Error al obtener la información: {e}")
+        return None
 
-# Verifica si se encontraron elementos en la búsqueda de "sell-value"
-if sell_elements:
-    # El primer elemento de la lista de "sell-value" es el valor de "Venta"
-    dolar_info['Dolar BNA']['Venta'] = sell_elements[0].text.strip()
-    dolar_info['Dolar Blue']['Venta'] = sell_elements[1].text.strip()
-    dolar_info['Dolar MEP']['Venta'] = sell_elements[5].text.strip()
-
-# Imprime el diccionario
-print(dolar_info)
+# Ejemplo de uso de la función
+if __name__ == "__main__":
+    info_dolar = dolarArg()
+    if info_dolar:
+        print(info_dolar)
